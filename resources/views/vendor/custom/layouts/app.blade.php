438f1a406e1f82dd017b77bb615329389d8d18f7
Emmanuel <whitemaxwell5@gmail.com>
2023-05-31 21:41:22 +0100

    rest fb login

--- resources/views/vendor/custom/layouts/app.blade.php
+++ resources/views/vendor/custom/layouts/app.blade.php
@@ -41,7 +41,7 @@
 </head>
 
 <body>
-   
+
     <div class="page-wrapper">
         <header class="header header-10 header-intro-clearance">
             <div class="header-top">
@@ -953,6 +953,16 @@
     <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
 
     <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var loginFbButton = document.getElementById('login-fb');
+            loginFbButton.addEventListener('click', checkLoginState);
+        });
+        document.addEventListener('DOMContentLoaded', function() {
+            var loginFbButton = document.getElementById('signup-fb');
+            loginFbButton.addEventListener('click', checkLoginState);
+        });
+
+
         function statusChangeCallback(response) { // Called with the results from FB.getLoginStatus().
             console.log('statusChangeCallback');
             console.log(response); // The current login status of the person.
@@ -974,10 +984,10 @@
 
         window.fbAsyncInit = function() {
             FB.init({
-                appId: '{{env('FACEBOOK_CLIENT_ID')}}',
+                appId: '{{ env('FACEBOOK_CLIENT_ID') }}',
                 cookie: true, // Enable cookies to allow the server to access the session.
                 xfbml: true, // Parse social plugins on this webpage.
-                version: '{{env('FACEBOOK_CLIENT_VERSION')}}' // Use this Graph API version for this call.
+                version: '{{ env('FACEBOOK_CLIENT_VERSION') }}' // Use this Graph API version for this call.
             });
 
 
