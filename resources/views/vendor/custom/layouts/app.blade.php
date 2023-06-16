39703140d56bb6367d8b906fb7c617786f395830
Emmanuel <whitemaxwell5@gmail.com>
2023-06-16 17:54:35 +0100

    requested changes

--- resources/views/vendor/custom/layouts/app.blade.php
+++ resources/views/vendor/custom/layouts/app.blade.php
@@ -638,13 +638,7 @@
                                             <span class="widget-about-title">Got Question? Call us 24/7</span>
                                             <a href="tel:123456789">+0123 456 789</a>
                                         </div><!-- End .col-sm-6 -->
-                                        <div class="col-sm-6 col-md-8">
-                                            <span class="widget-about-title">Payment Method</span>
-                                            <figure class="footer-payments">
-                                                <img src="{{ asset('custom/assets/images/payments.png') }}"
-                                                    alt="Payment methods" width="272" height="20">
-                                            </figure><!-- End .footer-payments -->
-                                        </div><!-- End .col-sm-6 -->
+
                                     </div><!-- End .row -->
                                 </div><!-- End .widget-about-info -->
                             </div><!-- End .widget about-widget -->
@@ -664,21 +658,7 @@
                             </div><!-- End .widget -->
                         </div><!-- End .col-sm-4 col-lg-3 -->
 
-                        <div class="col-sm-4 col-lg-2">
-                            <div class="widget">
-                                <h4 class="widget-title">Customer Service</h4><!-- End .widget-title -->
-
-                                <ul class="widget-list">
-                                    <li><a href="#">Payment Methods</a></li>
-                                    <li><a href="#">Money-back guarantee!</a></li>
-                                    <li><a href="#">Returns</a></li>
-                                    <li><a href="#">Shipping</a></li>
-                                    <li><a href="#">Terms and conditions</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                </ul><!-- End .widget-list -->
-                            </div><!-- End .widget -->
-                        </div><!-- End .col-sm-4 col-lg-3 -->
-
+                       
                         <div class="col-sm-4 col-lg-2">
                             <div class="widget">
                                 <h4 class="widget-title">My Account</h4><!-- End .widget-title -->
