6d67b8615cb214cdf69eb6a61aa1c82677a1e5de
Emmanuel <whitemaxwell5@gmail.com>
2023-06-17 00:00:34 +0100

    fixed categories arragement

--- resources/views/vendor/custom/layouts/app.blade.php
+++ resources/views/vendor/custom/layouts/app.blade.php
@@ -38,6 +38,8 @@
     <link rel="stylesheet" href="{{ asset('custom/assets/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('custom/assets/css/skins/skin-demo-13.css') }}">
     <link rel="stylesheet" href="{{ asset('custom/assets/css/demos/demo-13.css') }}">
+    {{-- ICON --}}
+    
 </head>
 
 <body>
@@ -152,55 +154,7 @@
                                 <span class="wishlist-txt">Wishlist</span>
                             </a>
 
-                            <div class="dropdown cart-dropdown">
-                                <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false" data-display="static">
-                                    <i class="icon-shopping-cart"></i>
-                                    <span class="cart-count">0</span>
-                                    <span class="cart-txt">Cart</span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right">
-                                    <div class="dropdown-cart-products">
-                                        <div class="product">
-                                            <div class="product-cart-details">
-                                                <h4 class="product-title">
-                                                    <a href="product.html">Beige knitted elastic runner shoes</a>
-                                                </h4>
-
-                                                <span class="cart-product-info">
-                                                    <span class="cart-product-qty">1</span>
-                                                    x ₦84.00
-                                                </span>
-                                            </div><!-- End .product-cart-details -->
-
-                                            <figure class="product-image-container">
-                                                <a href="product.html" class="product-image">
-                                                    <img src="assets/images/products/cart/product-1.jpg"
-                                                        alt="product">
-                                                </a>
-                                            </figure>
-                                            <a href="#" class="btn-remove" title="Remove Product"><i
-                                                    class="icon-close"></i></a>
-                                        </div><!-- End .product -->
-
-
-                                    </div><!-- End .cart-product -->
-
-                                    <div class="dropdown-cart-total">
-                                        <span>Total</span>
-
-                                        <span class="cart-total-price">₦160.00</span>
-                                    </div><!-- End .dropdown-cart-total -->
-
-                                    <div class="dropdown-cart-action">
-                                        <a href="cart.html" class="btn btn-primary">View Cart</a>
-                                        <a href="checkout.html"
-                                            class="btn btn-outline-primary-2"><span>Checkout</span><i
-                                                class="icon-long-arrow-right"></i></a>
-                                    </div><!-- End .dropdown-cart-total -->
-                                </div><!-- End .dropdown-menu -->
-                            </div><!-- End .cart-dropdown -->
+                           
                         </div>
                     </div><!-- End .header-right -->
                 </div><!-- End .container -->
