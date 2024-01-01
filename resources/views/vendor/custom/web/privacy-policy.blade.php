42bb9afda0215e4ebcf1aae7d8579d06faa1848b
Emmanuel kufre <whitemaxwell5@gmail.com>
2024-01-01 14:54:47 +0100

    sal

--- resources/views/vendor/custom/web/privacy-policy.blade.php
+++ resources/views/vendor/custom/web/privacy-policy.blade.php
@@ -24,7 +24,7 @@
                 <div class="card card-box card-sm bg-light">
                     <div class="card-header" id="heading-1">
                         <h2 class="card-title">
-                            <a role="button"  href="#collapse-1" aria-expanded="true" aria-controls="collapse-1">
+                            <a role="button"  href="#" aria-expanded="true" aria-controls="collapse-1">
                                Please Read through Ecomart's privacy-policy to enable get the best out this platform
                             </a>
                         </h2>
@@ -46,24 +46,6 @@
         </div><!-- End .container -->
     </div><!-- End .page-content -->
 
-    <div class="cta cta-display bg-image pt-4 pb-4" style="background-image: url(assets/images/backgrounds/cta/bg-7.jpg);">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-10 col-lg-9 col-xl-7">
-                    <div class="row no-gutters flex-column flex-sm-row align-items-sm-center">
-                        <div class="col">
-                            <h3 class="cta-title text-white">If You Have More Questions</h3><!-- End .cta-title -->
-                            <p class="cta-desc text-white">Quisque volutpat mattis eros</p><!-- End .cta-desc -->
-                        </div><!-- End .col -->
-
-                        <div class="col-auto">
-                            <a href="contact.html" class="btn btn-outline-white"><span>CONTACT US</span><i class="icon-long-arrow-right"></i></a>
-                        </div><!-- End .col-auto -->
-                    </div><!-- End .row no-gutters -->
-                </div><!-- End .col-md-10 col-lg-9 -->
-            </div><!-- End .row -->
-        </div><!-- End .container -->
-    </div><!-- End .cta -->
 </main><!-- End .main -->
 
 @endsection
